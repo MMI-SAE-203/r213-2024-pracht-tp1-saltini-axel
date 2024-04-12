740b5550918951a4954f287451884a691ae48e20
saltini-axel <axel.saltini@edu.univ-fcomte.fr>
2024-04-12 14:49:16 +0200

    ajout de l'accordeon réalisé en td

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+
+const maVar = ref(0)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour accordeon !</h1>
-  
-</template>
+<p>Page Accordéon</p> 
+  <p>La variable vaut : {{ maVar }}</p>
+  <button @click="maVar = maVar === 1 ? 0 : 1">Montrer 1</button>
+  <img src="https://image.noelshack.com/fichiers/2020/52/6/1608998191-154387-full.png" v-show="maVar === 1" class="text-2xl"></img>
+  <button @click="maVar = maVar === 2 ? 0 : 2">Montrer 2</button>
+  <img src="https://www.leparisien.fr/resizer/Spvo6YENyFn9tPG7qwB5xTv0Oe4=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/GAVNVCNPXXK35KHU2XEPS6XXIM.jpg" v-show="maVar === 2" class="text-2xl"></img>
+</template> 
